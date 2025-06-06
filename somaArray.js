e090dbd2f9edc9f508b51fcd12dd67577f946136
Gabi Emi <gabiemi2021@gmail.com>
2025-06-06 16:16:03 -0300

    criei uma array fora da function e mostrei o resultado com o console.log

--- somaArray.js
+++ somaArray.js
@@ -1,9 +1,13 @@
 function somaArray (array) {
     let soma = 0;
 
-    for (let i = 1; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         soma += array[i];
     }
+    return soma;
 
-    
 }
+
+let arrayNumeros = [5, 3, 2];
+let resultado = somaArray(arrayNumeros);
+console.log(resultado);
